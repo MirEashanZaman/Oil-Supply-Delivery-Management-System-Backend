678f73168dfd179ed4d83aa6ad6b2ff92d7f534b
Nnusrat50 <nnusratsarker@gmail.com>
2026-06-21 22:01:46 +0600

    added supplier

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerModule } from './customer/customer.module';
+import { SupplierModule } from './supplier/supplier.module';
 
 @Module({
-  imports: [CustomerModule],
+  imports: [CustomerModule, SupplierModule],
   controllers: [AppController],
   providers: [AppService],
 })
