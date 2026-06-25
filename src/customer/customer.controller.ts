17201747f9dd48aa7d37b3eb28ecceed9a890921
EshanZaman <eshanzaman6699@gmail.com>
2026-06-25 23:22:14 +0600

    Updated code with new features

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get, Param, Query, Post, Body, Put, ValidationPipe, UsePipes } from "@nestjs/common";
+import { Controller, Get, Param, Query, Post, Body, Put, ValidationPipe, UsePipes, UseInterceptors, UploadedFile, Res } from "@nestjs/common";
 import { CustomerService } from "./customer.service"
 import { CustomerDTO } from "./customer.dto";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { diskStorage, MulterError } from 'multer';
+import type { Response } from 'express';
 
 @Controller('customer')
 export class CustomerController {
@@ -25,17 +28,40 @@ export class CustomerController {
         return this.customerService.getCustomerByIDandName(id, name);
     }
 
-    @UsePipes(new ValidationPipe())
     @Post('createcustomer')
-    createAdmin(@Body() adminData: CustomerDTO): CustomerDTO {
-        console.log(adminData.name)
-        return this.customerService.createCustomer(adminData);
+    @UsePipes(new ValidationPipe())
+    @UseInterceptors(FileInterceptor('myfile', {
+        fileFilter: (req, myfile, cb) => {
+            if (myfile.originalname.match(/^.*\.(jpg|webp|png|jpeg)$/))
+                cb(null, true);
+            else {
+                cb(new MulterError('LIMIT_UNEXPECTED_FILE', 'image'), false);
+            }
+        },
+        limits: { fileSize: 30000000 },
+        storage: diskStorage({
+            destination: './uploads',
+            filename: function (req, myfile, cb) {
+                cb(null, Date.now() + myfile.originalname)
+            },
+        })
+    }))
+
+    createCustomer(@UploadedFile() file: Express.Multer.File, @Body() customerData: CustomerDTO): CustomerDTO {
+        console.log(file?.filename);
+        customerData.filename = file?.filename;
+        return this.customerService.createCustomer(customerData);
     }
 
     @Put('updatecustomer/:id') //use for update data like forget password
     updateCustomer(@Param('id') id: number, @Body() customerData: CustomerDTO): CustomerDTO {
         console.log(customerData.name)
         return this.customerService.updateCustomer(id, customerData);
+    }
+
+    @Get('/getimage/:name')
+    getImages(@Param('name') name: string, @Res() res: Response) {
+        res.sendFile(name, { root: './uploads' })
     }
 
 }
--- src/customer/customer.dto.ts
+++ src/customer/customer.dto.ts
@@ -8,4 +8,5 @@ export class CustomerDTO {
     @IsEmail()
     uname?: number;
     password?: string;
+    filename?: string;
 }
