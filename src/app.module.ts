5cb1e9332acccd4f8509122e39ebb4b3ac5ee2f5
EshanZaman <eshanzaman6699@gmail.com>
2026-07-06 08:18:29 +0600

    Add TypeOrmModule configuration for PostgreSQL database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,21 @@ import { CustomerModule } from './customer/customer.module';
 import { SupplierModule } from './supplier/supplier.module';
 import { DealerModule } from './dealer/dealer.module';
 import { AdminModule } from './admin/admin.module';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 
 @Module({
-  imports: [AdminModule, CustomerModule, SupplierModule, DealerModule],
+  imports: [AdminModule, CustomerModule, SupplierModule, DealerModule, TypeOrmModule.forRoot(
+    {
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '12345',
+      database: 'Oil-Supply-Delivery-Management-System',//Change to your database name
+      autoLoadEntities: true,
+      synchronize: true,
+    }),],
+
   controllers: [AppController],
   providers: [AppService],
 })
