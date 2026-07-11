380b09b7c8db643c6fe2845d8e861da83cd42d37
EshanZaman <eshanzaman6699@gmail.com>
2026-07-11 10:58:34 +0600

    Refactor Order and Customer entities, update relationships, and enhance Customer service methods

---
+++ src/category/category.entity.ts
@@ -0,0 +1,11 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany } from 'typeorm';
+import { Product } from '../product/product.entity';
+@Entity()
+export class Category {
+    @PrimaryGeneratedColumn()
+    id?: number;
+    @Column()
+    name?: string;
+    @ManyToMany(() => Product, product => product.categories)
+    products?: Product[];
+}
