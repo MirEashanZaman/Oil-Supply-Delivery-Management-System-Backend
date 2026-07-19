f1393b7519b7e4110b52bddfc820b0ca0d61c073
EshanZaman <eshanzaman6699@gmail.com>
2026-07-19 22:27:35 +0600

    Update DealerDTO and export loginDTO

--- src/dealer/dealer.dto.ts
+++ src/dealer/dealer.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsOptional, IsString, IsBoolean, Matches } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString, IsBoolean, Matches, IsEmail } from "class-validator";
 
 export class DealerDTO {
     @IsOptional()
@@ -13,4 +13,26 @@ export class DealerDTO {
     @IsString()
     @Matches(/^\d+$/, { message: "Phone number must contain only numbers" })
     phone?: string;
+
+    @IsNotEmpty({ message: "Email is required" })
+    @IsEmail({}, { message: "Email input must contain @ and .xyz domain" })
+    @Matches(/^[^\s@]+@[^\s@]+\.xyz$/i, { message: "Email input must contain @ and .xyz domain" })
+    email?: string;
+
+    password?: string;
+    filename?: string;
+
+    @IsNotEmpty({ message: "NID can't be empty" })
+    @Matches(/^\d{10,17}$/, { message: "NID must be 10–17 digits" })
+    nid?: string;
+}
+
+export class loginDTO {
+    @IsNotEmpty({ message: "Email is required" })
+    @IsEmail()
+    email?: string;
+
+    @IsNotEmpty({ message: "Password is required" })
+    @IsString()
+    password?: string;
 }
