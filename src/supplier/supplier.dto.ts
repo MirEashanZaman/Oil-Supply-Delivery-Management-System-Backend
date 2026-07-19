8bb20e0b580939a284065bf09f642cfea7ba97a0
EshanZaman <eshanzaman6699@gmail.com>
2026-07-19 22:28:10 +0600

    Update SupplierDTO and export loginDTO

--- src/supplier/supplier.dto.ts
+++ src/supplier/supplier.dto.ts
@@ -1,4 +1,4 @@
-import { IsIn, IsInt, IsNotEmpty, IsString, Matches, MaxLength, Min, IsOptional } from "class-validator";
+import { IsIn, IsInt, IsNotEmpty, IsString, Matches, MaxLength, Min, IsOptional, IsEmail } from "class-validator";
 
 export class SupplierDTO {
     @IsNotEmpty()
@@ -17,4 +17,25 @@ export class SupplierDTO {
     @IsIn(['active', 'inactive'])
     status?: string;
 
+    @IsNotEmpty({ message: "Email is required" })
+    @IsEmail({}, { message: "Email input must contain @ and .xyz domain" })
+    @Matches(/^[^\s@]+@[^\s@]+\.xyz$/i, { message: "Email input must contain @ and .xyz domain" })
+    email?: string;
+
+    password?: string;
+    filename?: string;
+
+    @IsNotEmpty({ message: "NID can't be empty" })
+    @Matches(/^\d{10,17}$/, { message: "NID must be 10–17 digits" })
+    nid?: string;
+}
+
+export class loginDTO {
+    @IsNotEmpty({ message: "Email is required" })
+    @IsEmail()
+    email?: string;
+
+    @IsNotEmpty({ message: "Password is required" })
+    @IsString()
+    password?: string;
 }
