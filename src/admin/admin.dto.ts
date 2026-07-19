4a1fec8ebc965d09347c26dfe35bdf55f84e5a1f
EshanZaman <eshanzaman6699@gmail.com>
2026-07-19 22:27:07 +0600

    Update AdminDTO and export loginDTO

--- src/admin/admin.dto.ts
+++ src/admin/admin.dto.ts
@@ -2,10 +2,12 @@ import {
     IsOptional,
     IsString,
     MaxLength,
+    IsNotEmpty,
+    IsEmail,
+    Matches,
 } from "class-validator";
 
 export class AdminDTO {
-
     @IsOptional()
     @IsString({ message: "Country must be a string" })
     @MaxLength(30, {
@@ -13,4 +15,25 @@ export class AdminDTO {
     })
     country?: string;
 
+    @IsNotEmpty({ message: "Email is required" })
+    @IsEmail({}, { message: "Email input must contain @ and .xyz domain" })
+    @Matches(/^[^\s@]+@[^\s@]+\.xyz$/i, { message: "Email input must contain @ and .xyz domain" })
+    email?: string;
+
+    password?: string;
+    filename?: string;
+
+    @IsNotEmpty({ message: "NID can't be empty" })
+    @Matches(/^\d{10,17}$/, { message: "NID must be 10–17 digits" })
+    nid?: string;
+}
+
+export class loginDTO {
+    @IsNotEmpty({ message: "Email is required" })
+    @IsEmail()
+    email?: string;
+
+    @IsNotEmpty({ message: "Password is required" })
+    @IsString()
+    password?: string;
 }
