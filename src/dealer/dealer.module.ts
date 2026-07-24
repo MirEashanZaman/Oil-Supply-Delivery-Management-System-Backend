67aa7cc5ec75fac1708d7b6e77476f7620c337a4
EshanZaman <eshanzaman6699@gmail.com>
2026-07-24 21:02:16 +0600

    Configure MailerModule in DealerModule

--- src/dealer/dealer.module.ts
+++ src/dealer/dealer.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MailerModule } from '@nestjs-modules/mailer';
 import { Dealer } from './dealer.entity';
 import { DealerService } from './dealer.service';
 import { DealerController } from './dealer.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Dealer])],
+  imports: [
+    TypeOrmModule.forFeature([Dealer]),
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com',
+        port: 465,
+        ignoreTLS: true,
+        secure: true,
+        auth: {
+          user: 'your gmail account',
+          pass: 'generated password',
+        },
+      },
+    }),
+  ],
   controllers: [DealerController],
   providers: [DealerService],
   exports: [DealerService],
