ff3cbb0e506fec12d671008b5d0157bcd182dd71
EshanZaman <eshanzaman6699@gmail.com>
2026-07-24 20:34:02 +0600

    Add PaymentModule

---
+++ src/payment/payment.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PaymentEntity } from './payment.entity';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([PaymentEntity])],
+    exports: [TypeOrmModule],
+})
+export class PaymentModule { }
