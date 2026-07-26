158fb3be35b9e1d1632865f3e216db5b5f421471
EshanZaman <eshanzaman6699@gmail.com>
2026-07-26 21:01:35 +0600

    Refactor: update src/customer/auth/auth.controller.ts

--- src/customer/auth/auth.controller.ts
+++ src/customer/auth/auth.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Post, UsePipes, UseInterceptors, UploadedFile, ValidationPipe, Res } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, UseInterceptors, UploadedFile, ValidationPipe, Res, BadRequestException } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CustomerDTO, loginDTO } from 'src/customer/customer.dto';
+import { CustomerDTO, loginDTO } from '../customer.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { MulterError, diskStorage } from 'multer';
 import * as express from 'express';
@@ -30,15 +30,21 @@ export class AuthController {
     ))
     @UsePipes(new ValidationPipe)
     async addUser(@Body() myobj: CustomerDTO, @UploadedFile() myfile: Express.Multer.File): Promise<CustomerDTO> {
+        if (!myfile?.filename) {
+            throw new BadRequestException('Photo is required during registration');
+        }
         if (!myobj.password) {
             myobj.password = "";
         }
         const salt = await bcrypt.genSalt();
         const hashedpassword = await bcrypt.hash(myobj.password, salt);
-        myobj.password = hashedpassword;
-        myobj.filename = myfile.filename;
-        myobj.title = 'Customer';
-        return this.authService.signUp(myobj);
+        
+        const customer = myobj as CustomerDTO & { username?: string; filename?: string };
+        customer.username = customer.userName;
+        customer.password = hashedpassword;
+        customer.filename = myfile.filename;
+        customer.title = 'Customer';
+        return this.authService.signUp(customer);
     }
 
     @Post('signIn')
@@ -50,7 +56,7 @@ export class AuthController {
 
         res.cookie("access_token", result.access_token, {
             httpOnly: true,
-            sameSite: "none",
+            sameSite: "lax",
             secure: false,
             path: "/",
             maxAge: 300 * 60 * 1000,
