6a14ab8ba5e2d4740dcc2db0dea57489a0506d92
EshanZaman <eshanzaman6699@gmail.com>
2026-07-26 21:02:10 +0600

    Refactor: update src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.use(cookieParser());
 
   app.enableCors({
-    origin: 'http://localhost:5000',
+    origin: 'http://localhost:8000',
     credentials: true,
   });
-  await app.listen(5000);
+  await app.listen(8000);
 
 }
 bootstrap();
