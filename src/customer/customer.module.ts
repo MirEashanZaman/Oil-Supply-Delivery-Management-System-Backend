00385a2f6e47064f401ed4f0b28cc49d4971b27e
EshanZaman <eshanzaman6699@gmail.com>
2026-07-26 21:01:49 +0600

    Refactor: update src/customer/customer.module.ts

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -6,11 +6,13 @@ import { CustomerEntity } from "./customer.entity";
 import { OrderEntity } from '../order/order.entity';
 import { OrderDetailsEntity } from '../order/order-details.entity';
 import { Product } from '../product/product.entity';
+import { PaymentEntity } from '../payment/payment.entity';
+import { DeliveryEntity } from '../delivery/delivery.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([CustomerEntity, OrderEntity, OrderDetailsEntity, Product]),
+        TypeOrmModule.forFeature([CustomerEntity, OrderEntity, OrderDetailsEntity, Product, PaymentEntity, DeliveryEntity]),
         MailerModule.forRoot({
             transport: {
                 host: 'smtp.gmail.com',
