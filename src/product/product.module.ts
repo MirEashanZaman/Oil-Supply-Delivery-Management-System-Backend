6feef05c9845059ff0d730567fc3ca00512c88cd
EshanZaman <eshanzaman6699@gmail.com>
2026-09-13 00:40:27 +0600

    Update src/product/product.module.ts

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -4,9 +4,10 @@ import { Product } from './product.entity';
 import { Category } from '../category/category.entity';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { OrderEntity } from '../order/order.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Product, Category])],
+    imports: [TypeOrmModule.forFeature([Product, Category, OrderEntity])],
     controllers: [ProductController],
     providers: [ProductService],
     exports: [ProductService, TypeOrmModule],
